c97ffeaa5e67b550e3821c61577f327b7fde747e
wilmerxx <wmshagnay@espe.edu.ec>
2022-02-02 19:04:39 -0500

    ingreso de componentes button

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule ({
     imports: [
       MatSliderModule,
+      MatButtonModule
     ],
     exports:[
      MatSliderModule,
+     MatButtonModule
     ]
   })
- 
+
 export class MateriaModule{}
